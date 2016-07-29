a202be5ccbc955f7996dc668bed0af517d59b78f
TwOq <lizq@shitou.local>
2016-07-29 16:55:15 +0800

    fix category  use dispatch once

--- AddressBookDemo/AddressBookDemo/Tool/UITableView+LQTableView.m
+++ AddressBookDemo/AddressBookDemo/Tool/UITableView+LQTableView.m
@@ -15,28 +15,32 @@
 
 + (void)load {
 
-    Class class = [self class];
-    SEL titleSelector = @selector(LQsectionIndexChangedToIndex:title:);
-    Method titleMethod = class_getInstanceMethod(class, titleSelector);
-    SEL touchSelector = @selector(LQsectionIndexTouchesEnded:);
-    Method touchMethod = class_getInstanceMethod(class, touchSelector);
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        SEL titleSelector = @selector(LQsectionIndexChangedToIndex:title:);
+        Method titleMethod = class_getInstanceMethod(class, titleSelector);
+        SEL touchSelector = @selector(LQsectionIndexTouchesEnded:);
+        Method touchMethod = class_getInstanceMethod(class, touchSelector);
 
-    unsigned int count;
-    Method *mothods = class_copyMethodList(class, &count);
-    for (int i = 0; i < count ;i++) {
-        Method method = mothods[i];
-        SEL methodSel = method_getName(method);
-        const char *name = sel_getName(methodSel);
-        NSString *methodString = [NSString stringWithCString:name encoding:NSUTF8StringEncoding];
-        if ([methodString isEqualToString:@"_sectionIndexChangedToIndex:title:"]) {
-            Method originalMethod = class_getInstanceMethod(class, methodSel);
-            method_exchangeImplementations(originalMethod, titleMethod);
+        unsigned int count;
+        Method *mothods = class_copyMethodList(class, &count);
+        for (int i = 0; i < count ;i++) {
+            Method method = mothods[i];
+            SEL methodSel = method_getName(method);
+            const char *name = sel_getName(methodSel);
+            NSString *methodString = [NSString stringWithCString:name encoding:NSUTF8StringEncoding];
+            if ([methodString isEqualToString:@"_sectionIndexChangedToIndex:title:"]) {
+                Method originalMethod = class_getInstanceMethod(class, methodSel);
+                method_exchangeImplementations(originalMethod, titleMethod);
+            }
+            if ([methodString isEqualToString:@"_sectionIndexTouchesEnded:"]) {
+                Method originalMethod = class_getInstanceMethod(class, methodSel);
+                method_exchangeImplementations(originalMethod, touchMethod);
+            }
         }
-        if ([methodString isEqualToString:@"_sectionIndexTouchesEnded:"]) {
-            Method originalMethod = class_getInstanceMethod(class, methodSel);
-            method_exchangeImplementations(originalMethod, touchMethod);
-        }
-    }
+
+    });
 
 }
 
